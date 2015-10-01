864aae7291af0747ee6fd3016915ca5ddd2723ec
bingoohuang <bingoo.huang@gmail.com>
2015-10-01 19:31:56 +0800

    refactor

--- src/main/java/com/github/bingoohuang/blackcat/sdk/utils/BlackCats.java
+++ src/main/java/com/github/bingoohuang/blackcat/sdk/utils/BlackCats.java
@@ -1,12 +1,30 @@
 package com.github.bingoohuang.blackcat.sdk.utils;
 
+import com.google.common.base.Splitter;
+import org.n3r.diamond.client.Miner;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Blackcats {
+    public static String readDiamond(String axis) {
+        List<String> parts = Splitter.on('^').splitToList(axis);
+        if (parts.size() == 1) {
+            return new Miner().getString(parts.get(0));
+        } else if (parts.size() == 2) {
+            return new Miner().getStone(parts.get(0), parts.get(1));
+        } else if (parts.size() == 3) {
+            return new Miner().getProperties(parts.get(0), parts.get(1))
+                    .getProperty(parts.get(2));
+        }
+
+        return null;
+    }
+
     public static String now() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
@@ -21,7 +39,7 @@ public class Blackcats {
     }
 
     public static String humanReadableByteCount(long bytes) {
-        return humanReadableByteCount(bytes, false);
+        return humanReadableByteCount(bytes, true);
     }
 
     // si 是否是十进制前缀 (SI), SI units and binary units
@@ -30,7 +48,7 @@ public class Blackcats {
         if (bytes < unit) return bytes + " B";
         int exp = (int) (Math.log(bytes) / Math.log(unit));
         String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
-        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+        return String.format("%.1f%sB", bytes / Math.pow(unit, exp), pre);
     }
 
     public static String getHostname() {
