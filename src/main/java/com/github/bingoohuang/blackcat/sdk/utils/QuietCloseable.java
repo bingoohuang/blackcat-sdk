e26c19917f814784a1fb9fb6cf35fb8edab85e33
bingoohuang <bingoo.huang@gmail.com>
2017-01-18 11:42:15 +0800

    refactor

---
+++ src/main/java/com/github/bingoohuang/blackcat/sdk/utils/QuietCloseable.java
@@ -0,0 +1,8 @@
+package com.github.bingoohuang.blackcat.sdk.utils;
+
+/**
+ * @author bingoohuang [bingoohuang@gmail.com] Created on 2017/1/17.
+ */
+public interface QuietCloseable {
+    void close();
+}
